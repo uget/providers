7ffb940057b795d3102aa8761358c0c9a4de7e1d
Danyel Bayraktar <danyel@webhippie.de>
2015-04-09 04:52:02 +0200

    Login to uploaded.net and read cookie. Todo: Store this cookie! (encrypt it?)

--- uploaded_net/ul_provider.go
+++ uploaded_net/ul_provider.go
@@ -1,12 +1,14 @@
 package uploaded_net
 
 import (
+	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	log "github.com/cihub/seelog"
 	"github.com/uget/uget/core"
 	"github.com/uget/uget/core/action"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"strings"
 )
@@ -17,9 +19,54 @@ func (p Provider) Name() string {
 	return "uploaded.net"
 }
 
+type Credentials struct {
+	LoginCookie string `json:"login_cookie"`
+}
+
 func (p Provider) Login(d *core.Downloader) {
 	// u, _ := url.Parse("http://uploaded.net")
+
 	// TODO: Read sensitive user information from somewhere and set client cookies.
+	// e.g.:
+	// credentials := Credentials{}
+	// accounts := account.Get(p.Name(), &credentials)
+}
+
+func login(id string, pw string) (*http.Cookie, error) {
+	reader := strings.NewReader(fmt.Sprintf("id=%s&pw=%s", id, pw))
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{Jar: jar}
+	u, _ := url.Parse("https://uploaded.net/io/login")
+	req, _ := http.NewRequest("POST", u.String(), reader)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "login" {
+			return cookie, nil
+		}
+	}
+	return nil, errors.New("Could not find login cookie in response headers.")
+}
+
+func (p Provider) AddAccount(prompter core.Prompter) {
+	prompter.Define(&core.Field{"id", "username", false, ""})
+	prompter.Define(&core.Field{"password", "password", true, ""})
+	values := prompter.Get()
+	id := values["id"]
+	pw := values["password"]
+
+	// do the request
+	cookie, err := login(id, pw)
+	_ = cookie
+	if err != nil {
+		prompter.Error(err.Error())
+	} else {
+		// account.Add(p.Name(), id, Credentials{cookie.Value})
+		prompter.Success()
+	}
 }
 
 func (p Provider) Action(r *http.Response, d *core.Downloader) *action.Action {
