2654eaaee003e916a054dcce5ba85456743afc43
Danyel Bayraktar <cydrop@gmail.com>
2018-01-01 15:41:09 +0800

    rapidgator: Normalize all URLs. Err if URL doesn't lead to a file.

--- rapidgator/rapidgator.go
+++ rapidgator/rapidgator.go
@@ -30,13 +30,20 @@ type file struct {
 	md5      string
 	url      *url.URL
 }
-type session struct {
-	sid     string
-	expires time.Time
-}
+
+const (
+	infoURL  = "https://rapidgator.net/api/file/info?sid=%s&url=%s"
+	loginURL = "https://rapidgator.net/api/user/login?username=%s&password=%s"
+	user     = "uget@dispostable.com"
+	password = "public8)"
+)
 
 var mtx = sync.Mutex{}
-var sess = session{}
+
+var session = struct {
+	sid     string
+	expires time.Time
+}{}
 
 func (f file) URL() *url.URL {
 	return f.url
@@ -54,13 +61,6 @@ func (f file) Checksum() (string, string, hash.Hash) {
 	return f.md5, "MD5", md5.New()
 }
 
-const (
-	infoURL  = "https://rapidgator.net/api/file/info?sid=%s&url=%s"
-	loginURL = "https://rapidgator.net/api/user/login?username=%s&password=%s"
-	user     = "uget@dispostable.com"
-	password = "public8)"
-)
-
 func (r rapidgator) Name() string {
 	return "rapidgator.net"
 }
@@ -72,7 +72,7 @@ func (r rapidgator) CanResolve(u *url.URL) bool {
 func refreshSession(c *http.Client) error {
 	mtx.Lock()
 	defer mtx.Unlock()
-	if sess.expires.Before(time.Now()) {
+	if session.expires.Before(time.Now()) {
 		m, code, err := request(c, fmt.Sprintf(loginURL, user, password))
 		if err != nil {
 			return err
@@ -80,8 +80,8 @@ func refreshSession(c *http.Client) error {
 		if code != 200 {
 			return fmt.Errorf("[rapidgator.net] status code %v when getting session key", code)
 		}
-		sess.sid = m["session_id"].(string)
-		sess.expires = time.Now().Add(5 * time.Minute)
+		session.sid = m["session_id"].(string)
+		session.expires = time.Now().Add(5 * time.Minute)
 	}
 	return nil
 }
@@ -108,22 +108,38 @@ func request(c *http.Client, url string) (map[string]interface{}, int, error) {
 	return m, code, nil
 }
 
+func normalize(u *url.URL) *url.URL {
+	htmlExt := ".html"
+	for strings.HasSuffix(u.Path, htmlExt) {
+		u2, _ := url.Parse("/")
+		*u2 = *u
+		u2.Path = u2.Path[0 : len(u2.Path)-len(htmlExt)]
+		u = u2
+	}
+	return u
+}
+
 func (r rapidgator) Resolve(u *url.URL) (core.File, error) {
+	paths := strings.Split(u.RequestURI(), "/")[1:]
+	if paths[0] != "file" {
+		return nil, fmt.Errorf("url doesn't point to a file")
+	}
+	u = normalize(u)
 	c := &http.Client{}
 	// Double check -- because we want to spend as little time as possible in critical section
-	if sess.expires.Before(time.Now()) {
+	if session.expires.Before(time.Now()) {
 		err := refreshSession(c)
 		if err != nil {
 			return nil, err
 		}
 	}
-	f, code, err := request(c, fmt.Sprintf(infoURL, sess.sid, u.String()))
+	f, code, err := request(c, fmt.Sprintf(infoURL, session.sid, u.String()))
 	if code != 200 {
 		if code == 404 {
 			return file{size: -1, url: u}, nil
 		} else if code == 401 { // session expired already?
 			// thread unsafe but we don't care if multiple goroutines invalidate the session
-			sess.expires = time.Now().Add(-100 * time.Hour)
+			session.expires = time.Now().Add(-100 * time.Hour)
 			return r.Resolve(u)
 		}
 		return nil, fmt.Errorf("[rapidgator.net] status code %v", code)
@@ -131,12 +147,6 @@ func (r rapidgator) Resolve(u *url.URL) (core.File, error) {
 	size, err := strconv.ParseInt(f["size"].(string), 10, 0)
 	if err != nil {
 		return nil, err
-	}
-	if strings.HasSuffix(u.Path, ".html") {
-		u2, _ := url.Parse("/")
-		*u2 = *u
-		u2.Path = u2.Path[0 : len(u2.Path)-5]
-		u = u2
 	}
 	return file{f["filename"].(string), size, f["hash"].(string), u}, nil
 }
