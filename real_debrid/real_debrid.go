44bb33272d5c60b516ec355648aca270e93f55f8
Danyel Bayraktar <cydrop@gmail.com>
2018-01-02 15:04:47 +0800

    real-debrid: Implement retrieve (for files of existing providers).

--- real_debrid/real_debrid.go
+++ real_debrid/real_debrid.go
@@ -4,13 +4,20 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
+	"github.com/uget/providers/oboom"
+	"github.com/uget/providers/rapidgator"
+	"github.com/uget/providers/uploaded_net"
 	"github.com/uget/uget/core"
 )
+
+const apiBase = "https://api.real-debrid.com/rest/1.0"
 
 type credentials struct {
 	Username string    `json:"username"`
@@ -21,11 +28,15 @@ type credentials struct {
 	APIToken string    `json:"apitoken" sensitive:"true"`
 }
 
-var _ core.Account = credentials{} // verify that credentials implements interface
+var _ core.Account = &credentials{} // verify that credentials implements interface
 
 var _ core.Accountant = &Provider{} // verify that realDebrid implements interface
 
-type Provider struct{}
+var _ core.Configured = &Provider{} // verify that provider implements interface
+
+type Provider struct {
+	mgr *core.AccountManager
+}
 
 func (c credentials) ID() string {
 	return c.Username
@@ -35,10 +46,58 @@ func (c credentials) String() string {
 	return fmt.Sprintf("real-debrid.com<username: %s, email: %s, premium: %v, fidelity: %v>", c.Username, c.Email, c.Premium, c.Points)
 }
 
-const apiBase = "https://api.real-debrid.com/rest/1.0"
+func (p *Provider) Configure(c *core.Config) {
+	p.mgr = c.AccountManager
+}
 
 func (p *Provider) Name() string {
 	return "real-debrid.com"
+}
+
+func (p *Provider) CanRetrieve(f core.File) uint {
+	if (&uploaded_net.Provider{}).CanResolve(f.URL()) ||
+		(&rapidgator.Provider{}).CanResolve(f.URL()) ||
+		(&oboom.Provider{}).CanResolve(f.URL()) {
+		selected, _ := p.mgr.SelectedAccount()
+		if selected != nil {
+			acc := selected.(*credentials)
+			if acc.Premium && acc.Expires.Before(time.Now()) {
+				// Prefer this as it has unlimited traffic
+				return 500
+			}
+		}
+	}
+	return 0
+}
+
+func (p *Provider) Retrieve(f core.File) (io.ReadCloser, error) {
+	selected, _ := p.mgr.SelectedAccount()
+	if selected == nil {
+		return nil, fmt.Errorf("no account exists")
+	}
+	acc := selected.(*credentials)
+	c := &http.Client{}
+	data := fmt.Sprintf("link=%s", url.QueryEscape(f.URL().String()))
+	req, _ := http.NewRequest("POST", apiBase+"/unrestrict/link", strings.NewReader(data))
+	req.Header.Set("Authorization", "Bearer "+acc.APIToken)
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var m map[string]interface{}
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		return nil, err
+	}
+	download, err := c.Get(m["download"].(string))
+	if err != nil {
+		return nil, err
+	}
+	return download.Body, nil
 }
 
 func (p *Provider) NewAccount(prompter core.Prompter) (core.Account, error) {
