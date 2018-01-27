512fbc8d6bfd654753e4f37b16970f21c2df8ee5
Danyel Bayraktar <cydrop@gmail.com>
2018-01-27 13:36:43 +0100

    Register newly supported providers

--- providers.go
+++ providers.go
@@ -2,10 +2,13 @@ package providers
 
 import (
 	"github.com/uget/providers/basic"
+	"github.com/uget/providers/nitroflare"
 	"github.com/uget/providers/oboom"
 	"github.com/uget/providers/rapidgator"
 	"github.com/uget/providers/real_debrid"
+	"github.com/uget/providers/share_online"
 	"github.com/uget/providers/uploaded"
+	"github.com/uget/providers/zippyshare"
 	"github.com/uget/uget/core"
 	"github.com/uget/uget/core/api"
 )
@@ -14,6 +17,9 @@ var All = []api.Provider{
 	&oboom.Provider{},
 	&rapidgator.Provider{},
 	&uploaded.Provider{},
+	&zippyshare.Provider{},
+	&share_online.Provider{},
+	&nitroflare.Provider{},
 	&real_debrid.Provider{},
 	&basic.Provider{},
 }
