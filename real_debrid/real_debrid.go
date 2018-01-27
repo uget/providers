e231c98e416eac954b7bc7987c8e4ed06ec6d2ac
Danyel Bayraktar <cydrop@gmail.com>
2018-01-27 12:52:55 +0100

    real-debrid: Hard-code supported domains.

--- real_debrid/real_debrid.go
+++ real_debrid/real_debrid.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"sync/atomic"
 	"time"
 
 	"github.com/Sirupsen/logrus"
@@ -54,33 +53,12 @@ func (p *Provider) Name() string {
 	return "real-debrid.com"
 }
 
-var domains = []string{}
-var domainsGotten int32
-
 func matches(host string) bool {
-	if atomic.CompareAndSwapInt32(&domainsGotten, 0, 1) {
-		c := &http.Client{}
-		resp, err := c.Get(apiBase + "/hosts/domains")
-		if err != nil {
-			goto DomainsErr
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			goto DomainsErr
-		}
-		err = json.Unmarshal(body, &domains)
-		if err != nil {
-			goto DomainsErr
-		}
-	}
 	for _, domain := range domains {
 		if strings.HasSuffix(host, domain) {
 			return true
 		}
 	}
-	return false
-DomainsErr:
-	atomic.StoreInt32(&domainsGotten, 0)
 	return false
 }
 
